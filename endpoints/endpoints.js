99729b7bd3429f3565f99e3162b6a93c9f15516a
YamaguchiAlan <alanyamaguchi72@gmail.com>
2022-04-02 04:34:28 -0300

    url changed

--- endpoints/endpoints.js
+++ endpoints/endpoints.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 const secret = process.env.NEXT_PUBLIC_HEADER_SECRET
 
 const api = axios.create({
-    baseURL: "https://server.yamabot.tk/api",
+    baseURL: "https://server.yamabot.tk:443/api",
     headers: {
       common: {
         'Origin-Auth-Secret': secret
